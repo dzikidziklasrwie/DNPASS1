1b8f7528790740787ad943fde294a9fffba2f906
Daniel <danielmaks400@gmail.com>
2025-09-09 21:58:09 +0200

    dass

--- InMemory/CommentInMemory.cs
+++ InMemory/CommentInMemory.cs
@@ -1,6 +1,57 @@
-﻿namespace InMemory;
+﻿using Entities;
+using RepositoryContracts;
+namespace InMemory;
 
-public class CommentInMemory
+public class CommentInMemory: IComment
 {
-    
+    List<Comment> comments = new List<Comment>();
+    public Task<Comment> AddAsync(Comment post)
+    {
+        post.UserId = comments.Any()
+            ? comments.Max(p => p.UserId) + 1
+            : 1;
+        comments.Add(post);
+        return Task.FromResult(post);
+    }
+
+    public Task UpdateAsync(Comment post)
+    {
+        Comment? existingUser = comments.SingleOrDefault(p => p.UserId == post.UserId);
+        if (existingUser is null)
+        {
+            throw new InvalidOperationException(
+
+                $"Comment with ID '{post.UserId}' not found");
+        }
+
+        comments.Remove(existingUser);
+        comments.Add(post); 
+        return Task.CompletedTask;
+    }
+
+    public Task DeleteAsync(int id)
+    {
+        Comment? userToRemove = comments.SingleOrDefault(p => p.UserId == id);
+        if (userToRemove is null)
+        {
+            throw new InvalidOperationException( $"Comment with ID '{id}' not found");
+        } 
+        comments.Remove(userToRemove); 
+        return Task.CompletedTask;
+    }
+
+    public Task<Comment> GetSingleAsync(int id)
+    {
+        Comment? userToRetrieve = comments.SingleOrDefault(p => p.UserId == id); 
+        if (userToRetrieve is null) 
+        { 
+            throw new InvalidOperationException($"Comment with ID '{id}' not found"); 
+        } 
+        return Task.FromResult(userToRetrieve);
+    }
+
+    public IQueryable<Comment> GetMany()
+    {
+        return comments.AsQueryable();
+    }
 }
--- InMemory/UserInMemory.cs
+++ InMemory/UserInMemory.cs
@@ -1,6 +1,57 @@
-﻿namespace InMemory;
+﻿using Entities;
+using RepositoryContracts;
+namespace InMemory;
 
-public class UserInMemory
+public class UserInMemory: IUser
 {
-    
+    List<User> users = new List<User>();
+    public Task<User> AddAsync(User post)
+    {
+        post.Id = users.Any()
+            ? users.Max(p => p.Id) + 1
+            : 1;
+        users.Add(post);
+        return Task.FromResult(post);
+    }
+
+    public Task UpdateAsync(User post)
+    {
+        User? existingUser = users.SingleOrDefault(p => p.Id == post.Id);
+        if (existingUser is null)
+        {
+            throw new InvalidOperationException(
+
+                $"User with ID '{post.Id}' not found");
+        }
+
+        users.Remove(existingUser);
+        users.Add(post); 
+        return Task.CompletedTask;
+    }
+
+    public Task DeleteAsync(int id)
+    {
+        User? userToRemove = users.SingleOrDefault(p => p.Id == id);
+        if (userToRemove is null)
+        {
+            throw new InvalidOperationException( $"User with ID '{id}' not found");
+        } 
+        users.Remove(userToRemove); 
+        return Task.CompletedTask;
+    }
+
+    public Task<User> GetSingleAsync(int id)
+    {
+        User? userToRetrieve = users.SingleOrDefault(p => p.Id == id); 
+        if (userToRetrieve is null) 
+        { 
+            throw new InvalidOperationException($"User with ID '{id}' not found"); 
+        } 
+        return Task.FromResult(userToRetrieve);
+    }
+
+    public IQueryable<User> GetMany()
+    {
+        return users.AsQueryable();
+    }
 }
